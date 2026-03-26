63f7495294beb78a06f76acc9af0b36ed81fdf65
tobyrushton <tobyjrushton@icloud.com>
2026-03-26 11:56:21 +0000

    db: add migration commands

---
+++ cmd/migrate/main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/alecthomas/kong"
+	"github.com/tobyrushton/bexbox-pl/libs/config"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/uptrace/bun/migrate"
+)
+
+var registeredMigrations = migrate.NewMigrations()
+
+type CLI struct {
+	Init        InitCmd        `cmd:"" help:"Create migration tables"`
+	Migrate     MigrateCmd     `cmd:"" help:"Migrate database"`
+	Rollback    RollbackCmd    `cmd:"" help:"Rollback the last migration group"`
+	Lock        LockCmd        `cmd:"" help:"Lock migrations"`
+	Unlock      UnlockCmd      `cmd:"" help:"Unlock migrations"`
+	CreateGo    CreateGoCmd    `cmd:"" name:"create-go" help:"Create Go migration"`
+	CreateSQL   CreateSQLCmd   `cmd:"" name:"create-sql" help:"Create up and down SQL migrations"`
+	CreateTxSQL CreateTxSQLCmd `cmd:"" name:"create-tx-sql" help:"Create up and down transactional SQL migrations"`
+	Status      StatusCmd      `cmd:"" help:"Print migrations status"`
+	MarkApplied MarkAppliedCmd `cmd:"" name:"mark-applied" help:"Mark migrations as applied without actually running them"`
+}
+
+type InitCmd struct{}
+type MigrateCmd struct{}
+type RollbackCmd struct{}
+type LockCmd struct{}
+type UnlockCmd struct{}
+type StatusCmd struct{}
+type MarkAppliedCmd struct{}
+
+type CreateGoCmd struct {
+	Name []string `arg:"" optional:"" help:"Migration name parts"`
+}
+
+type CreateSQLCmd struct {
+	Name []string `arg:"" optional:"" help:"Migration name parts"`
+}
+
+type CreateTxSQLCmd struct {
+	Name []string `arg:"" optional:"" help:"Migration name parts"`
+}
+
+func main() {
+	migrator := newMigrator()
+	cli := CLI{}
+	ctx := kong.Parse(&cli)
+
+	err := runCommand(ctx.Command(), &cli, migrator)
+	ctx.FatalIfErrorf(err)
+}
+
+func runCommand(command string, cli *CLI, migrator *migrate.Migrator) error {
+	commandContext := context.Background()
+
+	switch command {
+	case "init":
+		return cli.Init.Run(commandContext, migrator)
+	case "migrate":
+		return cli.Migrate.Run(commandContext, migrator)
+	case "rollback":
+		return cli.Rollback.Run(commandContext, migrator)
+	case "lock":
+		return cli.Lock.Run(commandContext, migrator)
+	case "unlock":
+		return cli.Unlock.Run(commandContext, migrator)
+	case "create-go":
+		return cli.CreateGo.Run(commandContext, migrator)
+	case "create-sql":
+		return cli.CreateSQL.Run(commandContext, migrator)
+	case "create-tx-sql":
+		return cli.CreateTxSQL.Run(commandContext, migrator)
+	case "status":
+		return cli.Status.Run(commandContext, migrator)
+	case "mark-applied":
+		return cli.MarkApplied.Run(commandContext, migrator)
+	default:
+		return fmt.Errorf("unknown command: %s", command)
+	}
+}
+
+func newMigrator() *migrate.Migrator {
+	appConfig, err := config.MustLoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(appConfig.DBConnectionString)))
+	err = sqldb.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithEnabled(false),
+		bundebug.FromEnv(),
+	))
+
+	templateData := map[string]string{
+		"Prefix": "example_",
+	}
+
+	return migrate.NewMigrator(db, registeredMigrations, migrate.WithTemplateData(templateData))
+}
+
+func (c *InitCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	return migrator.Init(ctx)
+}
+
+func (c *MigrateCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	if err := migrator.Lock(ctx); err != nil {
+		return err
+	}
+	defer migrator.Unlock(ctx) //nolint:errcheck
+
+	group, err := migrator.Migrate(ctx)
+	if err != nil {
+		return err
+	}
+	if group.IsZero() {
+		fmt.Printf("there are no new migrations to run (database is up to date)\n")
+		return nil
+	}
+	fmt.Printf("migrated to %s\n", group)
+	return nil
+}
+
+func (c *RollbackCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	if err := migrator.Lock(ctx); err != nil {
+		return err
+	}
+	defer migrator.Unlock(ctx) //nolint:errcheck
+
+	group, err := migrator.Rollback(ctx)
+	if err != nil {
+		return err
+	}
+	if group.IsZero() {
+		fmt.Printf("there are no groups to roll back\n")
+		return nil
+	}
+	fmt.Printf("rolled back %s\n", group)
+	return nil
+}
+
+func (c *LockCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	return migrator.Lock(ctx)
+}
+
+func (c *UnlockCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	return migrator.Unlock(ctx)
+}
+
+func (c *CreateGoCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	name := strings.Join(c.Name, "_")
+	mf, err := migrator.CreateGoMigration(ctx, name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
+	return nil
+}
+
+func (c *CreateSQLCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	name := strings.Join(c.Name, "_")
+	files, err := migrator.CreateSQLMigrations(ctx, name)
+	if err != nil {
+		return err
+	}
+
+	for _, mf := range files {
+		fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
+	}
+
+	return nil
+}
+
+func (c *CreateTxSQLCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	name := strings.Join(c.Name, "_")
+	files, err := migrator.CreateTxSQLMigrations(ctx, name)
+	if err != nil {
+		return err
+	}
+
+	for _, mf := range files {
+		fmt.Printf("created transaction migration %s (%s)\n", mf.Name, mf.Path)
+	}
+
+	return nil
+}
+
+func (c *StatusCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	ms, err := migrator.MigrationsWithStatus(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("migrations: %s\n", ms)
+	fmt.Printf("unapplied migrations: %s\n", ms.Unapplied())
+	fmt.Printf("last migration group: %s\n", ms.LastGroup())
+	return nil
+}
+
+func (c *MarkAppliedCmd) Run(ctx context.Context, migrator *migrate.Migrator) error {
+	group, err := migrator.Migrate(ctx, migrate.WithNopMigration())
+	if err != nil {
+		return err
+	}
+	if group.IsZero() {
+		fmt.Printf("there are no new migrations to mark as applied\n")
+		return nil
+	}
+	fmt.Printf("marked as applied %s\n", group)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/tobyrushton/bexbox-pl
 go 1.25.0
 
 require (
+	github.com/alecthomas/kong v1.14.0
 	github.com/jackc/pgx/v5 v5.9.1
 	github.com/sst/sst/v3 v3.19.3
 	github.com/uptrace/bun v1.2.18
 	github.com/uptrace/bun/dialect/pgdialect v1.2.18
+	github.com/uptrace/bun/driver/pgdriver v1.2.18
 	github.com/uptrace/bun/extra/bundebug v1.2.18
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -22,7 +25,11 @@ require (
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opentelemetry.io/otel v1.40.0 // indirect
+	go.opentelemetry.io/otel/trace v1.40.0 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
+	mellium.im/sasl v0.3.2 // indirect
 )
