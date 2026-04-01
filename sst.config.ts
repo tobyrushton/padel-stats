2a04763b692b5e693c004cd089d2a38e15f91efa
Toby Rushton <92679304+tobyrushton@users.noreply.github.com>
2026-04-01 17:41:59 +0100

    ci: deploy (#9)
    * db: add local development db
    
    * ci: add domain
    
    * ci: add deploy workflow

--- sst.config.ts
+++ sst.config.ts
@@ -6,40 +6,57 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
-      providers: { neon: "0.13.0" },
+      providers: { neon: "0.13.0", cloudflare: "6.13.1" },
     };
   },
   async run() {
-    const bexboxPL = new neon.Project("bexbox-pl", {
-      name: "bexbox-pl",
-      regionId: "aws-eu-west-2",
-      pgVersion: 17,
-      orgId: process.env.NEON_ORG_ID,
-      historyRetentionSeconds: 21600,
-    })
+    const domain = 
+      $app.stage === "production" ? "bexbox-padel.com" : "dev.bexbox-padel.com";
 
-    const db = new sst.Linkable("NeonDB", {
-      properties: {
-        connectionString: bexboxPL.connectionUri,
-      }
-    });
-
+    let db;
+    if ($app.stage === "production") {
+      const bexboxPL = new neon.Project("bexbox-pl", {
+        name: "bexbox-pl",
+        regionId: "aws-eu-west-2",
+        pgVersion: 17,
+        orgId: process.env.NEON_ORG_ID,
+        historyRetentionSeconds: 21600,
+      });
+      db = new sst.Linkable("NeonDB", {
+        properties: {
+          connectionString: bexboxPL.connectionUri,
+        },
+      });
+    } else {
+      db = new sst.Linkable("NeonDB", {
+        properties: {
+          connectionString:
+            "postgresql://admin:admin@localhost:5432/mydb?sslmode=disable",
+        },
+      });
+    }
     const api = new sst.aws.ApiGatewayV2("API", {
       link: [db],
-    })
-
+      domain: {
+        name: `api.${domain}`,
+        dns: sst.cloudflare.dns()
+      }
+    });
     api.route("$default", {
       handler: "github.com/tobyrushton/padel-stats/pkg/api/",
       runtime: "go",
       environment: {
         JWT_SECRET: process.env.JWT_SECRET!,
         JWT_ISSUER: process.env.JWT_ISSUER!,
-      }
-    })
-
+      },
+    });
     const web = new sst.aws.Astro("www", {
       link: [api],
       path: "pkg/www",
-    })
+      domain: {
+        name: domain,
+        dns: sst.cloudflare.dns()
+      }
+    });
   },
 });
