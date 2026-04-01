1f3ace26a6cdebbe4f2251ece681b85d1608109e
tobyrushton <tobyjrushton@icloud.com>
2026-04-01 17:30:38 +0100

    ci: add domain

--- sst.config.ts
+++ sst.config.ts
@@ -6,10 +6,12 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
-      providers: { neon: "0.13.0" },
+      providers: { neon: "0.13.0", cloudflare: "6.13.1" },
     };
   },
   async run() {
+    const domain = 
+      $app.stage === "production" ? "bexbox-padel.com" : "dev.bexbox-padel.com";
 
     let db;
     if ($app.stage === "production") {
@@ -19,37 +21,42 @@ export default $config({
         pgVersion: 17,
         orgId: process.env.NEON_ORG_ID,
         historyRetentionSeconds: 21600,
-      })
-
+      });
       db = new sst.Linkable("NeonDB", {
         properties: {
           connectionString: bexboxPL.connectionUri,
-        }
+        },
       });
     } else {
       db = new sst.Linkable("NeonDB", {
         properties: {
-          connectionString: "postgresql://admin:admin@localhost:5432/mydb?sslmode=disable",
-        }
-      })
+          connectionString:
+            "postgresql://admin:admin@localhost:5432/mydb?sslmode=disable",
+        },
+      });
     }
-
     const api = new sst.aws.ApiGatewayV2("API", {
       link: [db],
-    })
-
+      domain: {
+        name: `api.${domain}`,
+        dns: sst.cloudflare.dns()
+      }
+    });
     api.route("$default", {
       handler: "github.com/tobyrushton/padel-stats/pkg/api/",
       runtime: "go",
       environment: {
         JWT_SECRET: process.env.JWT_SECRET!,
         JWT_ISSUER: process.env.JWT_ISSUER!,
-      }
-    })
-
+      },
+    });
     const web = new sst.aws.Astro("www", {
       link: [api],
       path: "pkg/www",
-    })
+      domain: {
+        name: domain,
+        dns: sst.cloudflare.dns()
+      }
+    });
   },
 });
